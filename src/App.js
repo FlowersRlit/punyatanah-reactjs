e204a6cc727e0918bbee64d6535aa97b55f8c089
FlowersRlit <yourstrulygurl@gmail.com>
2023-04-20 11:36:30 +0700

    routed to navigate to About Us page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import DetailsItem from "./pages/Marketplace/children/DetailsItem";
 import Marketplace from "./pages/Marketplace/Marketplace";
 import MarketplaceMain from "./pages/Marketplace/Main/MarketplaceMain";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/marketplace" element={<MarketplaceMain />} />
         <Route path="/marketplace/catalog" element={<Marketplace />} />
         <Route path="/marketplace/catalog/:id" element={<DetailsItem />} />
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRef , useEffect , useState} from "react";
+import { useRef, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import NavbarMidButtons from "./MiscComponents/NavbarMidButtons";
 import NavbarMobileButtons from "./MiscComponents/NavbarMobileButtons";
@@ -7,122 +7,120 @@ import Logo from "../assets/navbar.svg";
 import { useSpring, animated } from "@react-spring/web";
 
 const Navbar = () => {
-	const [prevYPos, setPrevYPos] = useState(window.pageYOffset);
+  const [prevYPos, setPrevYPos] = useState(window.pageYOffset);
 
-	const [springs, api] = useSpring(() => ({
-		from: { x: 0 },
-	}))
+  const [springs, api] = useSpring(() => ({
+    from: { x: 0 },
+  }));
 
-	const onScrollUp = () => {
-		api.start({
-			to: {
-				y: 0,
-			},
-		})
-	}
+  const onScrollUp = () => {
+    api.start({
+      to: {
+        y: 0,
+      },
+    });
+  };
 
-	const onScrollDown = () => {
-		api.start({
-			to: {
-				y: -100,
-			},
-		})
-	}
+  const onScrollDown = () => {
+    api.start({
+      to: {
+        y: -100,
+      },
+    });
+  };
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const currentYPos = window.pageYOffset;
-			if (prevYPos > currentYPos || currentYPos < 10) {
-				onScrollUp()
-			} else onScrollDown();
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentYPos = window.pageYOffset;
+      if (prevYPos > currentYPos || currentYPos < 10) {
+        onScrollUp();
+      } else onScrollDown();
 
-			setPrevYPos(currentYPos);
-		};
-	
-		window.addEventListener('scroll', handleScroll);
-	
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, [prevYPos]);
+      setPrevYPos(currentYPos);
+    };
 
+    window.addEventListener("scroll", handleScroll);
 
-	const menu = useRef(null);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevYPos]);
 
+  const menu = useRef(null);
 
-	const handleClick = () => {
-		menu.current.classList.toggle("hidden");
-	};
+  const handleClick = () => {
+    menu.current.classList.toggle("hidden");
+  };
 
-	return (
-		<animated.div style={{...springs}} className={`bg-white fixed top-0 w-full z-50 bg-white ${prevYPos > 20 ? 'shadow-md': ''}`}>
-			<div className="mx-auto max-w-6xl px-4">
-				<div className="grid grid-cols-2 md:grid-cols-4">
-					<div className="flex space-x-4">
-						<div>
-							{/* logo */}
-							<NavLink to="./" className="flex items-center py-4 px-2">
-								<img src={Logo} alt="logo-navbar" />
-							</NavLink>
-						</div>
-					</div>
+  return (
+    <animated.div
+      style={{ ...springs }}
+      className={`fixed top-0 z-50 w-full bg-white  ${
+        prevYPos > 20 ? "shadow-md" : ""
+      }`}>
+      <div className="mx-auto max-w-6xl px-4">
+        <div className="grid grid-cols-2 md:grid-cols-4">
+          <div className="flex space-x-4">
+            <div>
+              {/* logo */}
+              <NavLink to="./" className="flex items-center py-4 px-2">
+                <img src={Logo} alt="logo-navbar" />
+              </NavLink>
+            </div>
+          </div>
 
-					{/* nav: middle buttons */}
-					<div className="hidden items-center space-x-1 font-medium md:flex col-span-2 justify-center">
-						<NavbarMidButtons Button="Home" Page="./" />
-						<NavbarMidButtons Button="Marketplace" Page="./marketplace" />
-						<NavbarMidButtons Button="Review" Page="./" />
-						<NavbarMidButtons Button="About Us" Page="./" />
-					</div>
+          {/* nav: middle buttons */}
+          <div className="col-span-2 hidden items-center justify-center space-x-1 font-medium md:flex">
+            <NavbarMidButtons Button="Home" Page="./" />
+            <NavbarMidButtons Button="Marketplace" Page="./marketplace" />
+            <NavbarMidButtons Button="Review" Page="./" />
+            <NavbarMidButtons Button="About Us" Page="./about" />
+          </div>
 
-					{/* sign in & sign up */}
-					<div className="hidden items-center justify-end space-x-1 md:flex">
-						<NavLink
-							to="./login"
-							className="py-2 px-3 font-medium text-[#016450] hover:text-[#FF6500]"
-						>
-							Sign In
-						</NavLink>
-						<NavLink
-							to="./register"
-							className="rounded-md border-[#016450] bg-[#016450] py-2 px-3 font-medium text-white hover:bg-[#FF6500] hover:text-white"
-						>
-							Sign Up
-						</NavLink>
-					</div>
+          {/* sign in & sign up */}
+          <div className="hidden items-center justify-end space-x-1 md:flex">
+            <NavLink
+              to="./login"
+              className="py-2 px-3 font-medium text-[#016450] hover:text-[#FF6500]">
+              Sign In
+            </NavLink>
+            <NavLink
+              to="./register"
+              className="rounded-md border-[#016450] bg-[#016450] py-2 px-3 font-medium text-white hover:bg-[#FF6500] hover:text-white">
+              Sign Up
+            </NavLink>
+          </div>
 
-					{/* mobile responsive */}
-					<div className="flex items-center md:hidden justify-end">
-						<button onClick={handleClick} className="mobile-menu-button">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth="1.5"
-								stroke="currentColor"
-								className="h-6 w-6 text-[#016450]"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-								/>
-							</svg>
-						</button>
-					</div>
-				</div>
-			</div>
+          {/* mobile responsive */}
+          <div className="flex items-center justify-end md:hidden">
+            <button onClick={handleClick} className="mobile-menu-button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6 text-[#016450]">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
 
-			{/* mobile burger box */}
-			<div
-				ref={menu}
-				className="mobile-menu hidden text-center shadow-md shadow-[#016450] md:hidden"
-			>
-				<NavbarMobileButtons Title="Home" Page="./" />
-				<NavbarMobileButtons Title="Marketplace" Page="./marketplace" />
-				<NavbarMobileButtons Title="Review" Page="./" />
-				<NavbarMobileButtons Title="About Us" Page="./" />
-			</div>
-		</animated.div>
-	);
+      {/* mobile burger box */}
+      <div
+        ref={menu}
+        className="mobile-menu hidden text-center shadow-md shadow-[#016450] md:hidden">
+        <NavbarMobileButtons Title="Home" Page="./" />
+        <NavbarMobileButtons Title="Marketplace" Page="./marketplace" />
+        <NavbarMobileButtons Title="Review" Page="./" />
+        <NavbarMobileButtons Title="About Us" Page="./" />
+      </div>
+    </animated.div>
+  );
 };
 
 export default Navbar;
