e4cf3a684191bc75298b31fdb00d2e99b19176a5
FlowersRlit <yourstrulygurl@gmail.com>
2023-05-20 22:00:59 +0700

    updated lorem ipsum text

--- src/components/childdetails/Description.js
+++ src/components/childdetails/Description.js
@@ -5,13 +5,24 @@ const Description = () => {
     <div className="py-8">
       <h1 className="px-8 text-2xl font-bold text-hijau1">Deskripsi tanah</h1>
       <p className="px-12 pt-4">
-        lorem20x lorem20x lorem20x lorem20x lorem20x lorem20x lorem20x
-        lorem20xasdasdasd asdasdasdasdasdasd asdasd asdasd lorem20x lorem20x
-        lorem20x lorem20x rem20x lorem20x lorem20x lorem20x lorem20x lorem20x
-        lorem20x lorem20x lorem20x lorem20x lorem20x lorem20x rem20x lorem20x
-        lorem20x lorem20x lorem20x lorem20x lorem20x lorem20x lorem20x lorem20x
-        lorem20x lorem20x rem20x lorem20x lorem20x lorem20x lorem20x lorem20x
-        lorem20x lorem20x lorem20x lorem20x lorem20x lorem20x
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quas quos
+        voluptatum voluptatem quae voluptatibus quam eius voluptatum sed fuga
+        repellendus voluptatibus quaerat voluptate molestiae voluptatibus
+        voluptatum quas quos voluptatum voluptatum quae voluptatibus quam eius
+        voluptatum sed fuga repellendus voluptatibus quaerat voluptatum quas
+        quos voluptatum voluptatum quae voluptatibus quam eius voluptatum sed
+        fuga repellendus voluptatibus quaerat voluptatum quas quos voluptatum
+        voluptatum quae voluptatibus quam eius voluptatum sed fuga repellendus
+        voluptatibus quaerat voluptatum quas quos voluptatum voluptatum quae
+        voluptatibus quam eius voluptatum sed fuga repellendus voluptatibus
+        quaerat voluptatum quas quos voluptatum voluptatum quae voluptatibus
+        quam eius voluptatum sed fuga repellendus voluptatibus quaerat
+        voluptatum quas quos voluptatum voluptatum quae voluptatibus quam eius
+        voluptatum sed fuga repellendus voluptatibus quaerat voluptatum quas
+        quos voluptatum voluptatum quae voluptatibus quam eius voluptatum sed
+        fuga repellendus voluptatibus quaerat voluptatum quas quos voluptatum
+        voluptatum quae voluptatibus quam eius voluptatum sed fuga repellendus
+        voluptatibus quaerat voluptatum quas quos voluptatum voluptatum quae
       </p>
     </div>
   );
